a54744422153d550d8c4416927e96eed8c07d044
Matthias Schenk <matthias.schenk@sevdesk.de>
2022-12-17 21:47:29 +0100

    [V-3] Added Detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     kotlin("jvm") version "1.7.22"
     id("io.ktor.plugin") version "2.2.1"
     id("org.sonarqube") version "3.5.0.2730"
+    id("io.gitlab.arturbosch.detekt") version "1.22.0"
     jacoco
 }
 
@@ -24,6 +25,9 @@ application {
 
 repositories {
     mavenCentral()
+    maven {
+        setUrl("https://plugins.gradle.org/m2/")
+    }
 }
 
 dependencies {
@@ -68,6 +72,11 @@ tasks.jacocoTestReport {
     }
 }
 
+tasks.sonar{
+    dependsOn(tasks.detekt)
+    dependsOn(tasks.test)
+}
+
 sonar {
     properties {
         property("sonar.projectKey", "gradle-custom-sourceset")
@@ -75,5 +84,45 @@ sonar {
         property("sonar.login", "sqp_63b03d57d145d99683ec1396a4ab423c39aa6fca")
         property("sonar.junit.reportPaths", "build/test-results/**/TEST-*.xml")
         property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/**/*.xml")
+        property("sonar.kotlin.detekt.reportPaths", "build/reports/detekt/detekt.xml")
     }
 }
+
+detekt {
+// Version of Detekt that will be used. When unspecified the latest detekt
+// version found will be used. Override to stay on the same version.
+    toolVersion = "1.22.0"
+
+    // The directories where detekt looks for source files.
+    // Defaults to `files("src/main/java", "src/test/java", "src/main/kotlin", "src/test/kotlin")`.
+    source = files("src/main/kotlin")
+    // Builds the AST in parallel. Rules are always executed in parallel.
+    // Can lead to speedups in larger projects. `false` by default.
+    parallel = false
+
+    // Define the detekt configuration(s) you want to use.
+    // Defaults to the default detekt configuration.
+    config = files("config/config.yml")
+
+    // Applies the config files on top of detekt's default config file. `false` by default.
+    buildUponDefaultConfig = false
+
+    // Turns on all the rules. `false` by default.
+    allRules = false
+
+    // Specifying a baseline file. All findings stored in this file in subsequent runs of detekt.
+    baseline = file("config/baseline.xml")
+
+    // Disables all default detekt rulesets and will only run detekt with custom rules
+    // defined in plugins passed in with `detektPlugins` configuration. `false` by default.
+    disableDefaultRuleSets = false
+
+    // Adds debug output during task execution. `false` by default.
+    debug = false
+
+    // If set to `true` the build does not fail when the
+    // maxIssues count was reached. Defaults to `false`.
+    ignoreFailures = false
+
+}
+
